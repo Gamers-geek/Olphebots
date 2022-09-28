ee3282c05cbeee33aa52ccd8873857d84999b2dd
Gamers-geek <contact@gamers-geek.gq>
2022-09-28 22:54:14 +0200

    Suppresion d'Olphepub

--- index.js
+++ index.js
@@ -3,4 +3,3 @@ const olphebot = require("./olphebot/index");
 const olphedia = require("./olphedia/index");
 const olphesupport = require("./olphesupport/index");
 const olphepartner = require("./olphepartner/index");
-const olphepub = require("./olphepub/index");
